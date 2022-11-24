f0b293a14ca235d0cfac2ae683513d6ae6ac7056
richard rosenblat <rosenblatr@gmail.com>
2022-11-24 00:12:32 -0300

    Criando o modelo Story

---
+++ stories/models/story.js
@@ -0,0 +1,26 @@
+"use strict";
+import ObjectID from "bson-objectid";
+import { Model } from "sequelize";
+export default (sequelize, DataTypes) => {
+  class Story extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(_models) {
+      // define association here
+    }
+  }
+  Story.init({
+    userId: DataTypes.STRING,
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    impressions: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Story',
+  });
+  Story.beforeCreate(story => story.id = ObjectID().toHexString());
+  return Story;
+};
